6c8d8e31abaea50aa3a8eb81df80fa32789ca8de
Andre M. Bertachini <bertachiniandre@gmail.com>
2025-10-19 15:16:12 -0300

    Prepare data for training, everything as Tensors

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+
 dataset = pandas.read_csv("dataset.csv")
 
 X = dataset.drop(columns=["status"])
@@ -16,3 +19,14 @@ X_train, X_test, y_train, y_test = train_test_split(
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+
+X_train_tensor = torch.FloatTensor(X_train_scaled)
+X_test_tensor = torch.FloatTensor(X_test_scaled)
+y_train_tensor = torch.LongTensor(y_train)
+y_test_tensor = torch.LongTensor(y_test)
+
+train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
