895ed96e9a68cf9aaea4ccb1077bf1537a8a9207
Andre M. Bertachini <bertachiniandre@gmail.com>
2025-10-19 21:27:28 -0300

    Finally train() and eval()

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pandas
-import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 import torch
 import torch.nn as nn
-# import torch.optim as optim
+import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
 
@@ -25,6 +24,36 @@ class Net(nn.Module):
         x = self.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
+def train(model, epochs, train_loader, criterion, optimizer):
+    model.train()
+    for e in range(epochs):
+        running_loss = 0.0
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        if (e + 1) % 10 == 0:
+            print(f"Epoch: {e+1}/{epochs} Loss: {running_loss/len(train_loader):.4f}")
+
+
+def eval(model, test_loader):
+    model.eval()
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = 100 * correct / total
+    print(f"Accuracy: {accuracy:.2f}%")
 
 
 if __name__ == "__main__":
@@ -52,5 +81,21 @@ if __name__ == "__main__":
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-    # criterion = nn.CrossEntropyLoss()
-    # optimizer = optim.Adam(model.)
+    """
+        Input igual a quantidade de features.
+        Ouput igual a quantidade de classes.
+        FC1 = 2 -> 64
+        FC2 = 64 -> 32
+        FC3 = 32 -> 2
+    """
+    input_size = X_train_scaled.shape[1]
+    hidden_size1 = 64
+    hidden_size2 = 32
+    output_size = len(y.unique())
+
+    model = Net(input_size, hidden_size1, hidden_size2, output_size)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    train(model, 1000, train_loader, criterion, optimizer)
+    eval(model, test_loader)
